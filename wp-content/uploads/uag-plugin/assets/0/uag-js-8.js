578790c644297c70197336245aeaf606adea203e
Joaquin Zubiria <embschapter@itba.edu.ar>
2026-05-17 17:32:09 -0300

    pagina test

---
+++ wp-content/uploads/uag-plugin/assets/0/uag-js-8.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function(){ window.addEventListener("DOMContentLoaded", function(){
+	var swiper = new Swiper( ".uagb-block-d015c5ce .uagb-swiper",
+		{"autoplay":{"delay":3000,"disableOnInteraction":false,"pauseOnMouseEnter":true,"stopOnLastSlide":false},"loop":true,"speed":800,"effect":"slide","direction":"horizontal","flipEffect":{"slideShadows":false},"fadeEffect":{"crossFade":true},"pagination":{"el":".uagb-block-d015c5ce .swiper-pagination","clickable":true,"hideOnClick":false},"navigation":{"nextEl":".uagb-block-d015c5ce .swiper-button-next","prevEl":".uagb-block-d015c5ce .swiper-button-prev"}}	);
+});
+
+ });
